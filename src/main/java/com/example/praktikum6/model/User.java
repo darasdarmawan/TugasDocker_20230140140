8ec0ae6cacaa188d3580c7ef88799b1f41f6136e
darasdarmawan <darasyauqidarmawan@gmail.com>
2026-04-27 21:09:22 +0700

    update model user

--- src/main/java/com/example/praktikum6/model/User.java
+++ src/main/java/com/example/praktikum6/model/User.java
@@ -1,13 +1,22 @@
 package com.example.praktikum6.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Builder
+@Table (name = "20230140140_users")
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    private String Id;
     private String nama;
     private String nim;
     private String jenisKelamin;
